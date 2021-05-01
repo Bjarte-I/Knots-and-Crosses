deb58df5c3bfb0884b539ac5ad0ae1e807890dd8
Bjarte Iversen <bjartewi@uia.no>
2021-05-01 15:46:38 +0200

    Can now create, play and finish a game. It will return to the main activity when the game is lost or won.

--- app/src/main/java/com/example/knotsandcrosses/GameActivity.kt
+++ app/src/main/java/com/example/knotsandcrosses/GameActivity.kt
@@ -1,18 +1,14 @@
 package com.example.knotsandcrosses
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.SystemClock
 import android.util.Log
 import com.example.knotsandcrosses.api.GameService
-import com.example.knotsandcrosses.api.GameServiceCallback
 import com.example.knotsandcrosses.api.data.Game
 import com.example.knotsandcrosses.databinding.ActivityGameBinding
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -21,6 +17,7 @@ class GameActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGameBinding
     private lateinit var state: Game
+    private var waiting = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,26 +31,54 @@ class GameActivity : AppCompatActivity() {
 
         setListenersOnButtons()
 
-        toggleAllGameButtons()
+        disableAllGameButtons()
 
         waitForOpponent()
     }
 
     private fun waitForOpponent(){
         CoroutineScope(IO).launch {
-            while(state.players.size != 2){
+            while(waiting){
                 Log.d("waitForOpponent", "Launching pollgame request")
                 GameService.pollGame(state.gameId, this@GameActivity::onPolledGame)
                 delay(5000)
             }
         }
+        waiting = true
     }
 
-    private fun foundOpponent(){
-        Log.d("FoundOpponent", "Found!")
+    private fun stateChanged(){
+        Log.d("StateChanged", "Changed!")
+        if(checkForWin("player2")){
+            Log.d("checkForWin", "You lost!")
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            return
+        }
         binding.tvOpponentName.text = state.players[1]
         binding.tvPlayerName.setTextColor(Color.GREEN)
-        toggleAllGameButtons()
+        binding.tvOpponentName.setTextColor(Color.BLACK)
+        var buttonsToBeEnabled = mutableListOf<Int>()
+        var index = 0
+        state.state.forEach{ row ->
+            row.forEach {
+                if(it == "0"){
+                    buttonsToBeEnabled.add(index)
+                }
+                index += 1
+            }
+        }
+        toggleValidGameButtons(buttonsToBeEnabled)
+    }
+
+    private fun toggleValidGameButtons(toBeEnabled: List<Int>){
+        binding.apply {
+            val gameButtons = listOf(zeroZero, zeroOne, zeroTwo, oneZero, oneOne, oneTwo, twoZero, twoOne, twoTwo)
+            toBeEnabled.forEach{
+                val button = gameButtons[it]
+                button.isEnabled = true  // Enable the buttons for places that has not been taken.
+            }
+        }
     }
 
 
@@ -64,19 +89,17 @@ class GameActivity : AppCompatActivity() {
         } else {
             if(newState != state){
                 state = newState
-            }
-            Log.d("polledGame", state.players.toString())
-            if(state.players.size == 2){
-                foundOpponent()
+                waiting = false
+                stateChanged()
             }
         }
     }
 
-    private fun toggleAllGameButtons(){
+    private fun disableAllGameButtons(){
         binding.apply {
             val gameButtons = listOf(zeroZero, zeroOne, zeroTwo, oneZero, oneOne, oneTwo, twoZero, twoOne, twoTwo)
             for (button in gameButtons){
-                button.isEnabled = !button.isEnabled
+                button.isEnabled = false
             }
         }
     }
@@ -95,20 +118,66 @@ class GameActivity : AppCompatActivity() {
     private fun clickedButton(index:Int){
         Log.d("clickedButton", index.toString())
         var row = -1
-        if(index < 9){
-            row = 2
-        }
-        if(index < 6){
-            row = 1
-        }
-        if(index < 3){
+        var indx = -1
+        if(index == 0 || index == 1 || index == 2){
             row = 0
+            indx = index
         }
-        state.state[row][index] = "X"
+        if(index == 3 || index == 4 || index == 5){
+            row = 1
+            indx = index - 3
+        }
+        if(index == 6 || index == 7  || index == 8){
+            row = 2
+            indx = index - 6
+        }
+        state.state[row][indx] = "X"
         GameService.updateGame(state.gameId, state.state, this::onUpdatedGame)
-        toggleAllGameButtons()
+        disableAllGameButtons()
         binding.tvOpponentName.setTextColor(Color.GREEN)
-        binding.tvPlayerName.setTextColor(Color.WHITE)
+        binding.tvPlayerName.setTextColor(Color.BLACK)
+        if(checkForWin("player1")){
+            Log.d("checkForWin", "You won!")
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            return
+        }
+        waitForOpponent()
+    }
+
+    private fun checkForWin(player: String): Boolean {
+        var compareParameter = ""
+        if(player == "player1"){
+            compareParameter = "X"
+        } else if(player == "player2") {
+            compareParameter = "O"
+        }
+        var oneBigRow = mutableListOf<Int>()
+        state.state.forEach { row ->
+            row.forEach {
+                if(it == compareParameter){
+                    oneBigRow.add(1)
+                } else {
+                    oneBigRow.add(0)
+                }
+            }
+        }
+        for(i in 0..2){
+            if(oneBigRow[3 * i] + oneBigRow[1 + 3 * i] + oneBigRow[2 + 3 * i] == 3){  // Horizontal win
+                Log.d("checkForWin", "A player won.")
+                return true
+            }
+            if(oneBigRow[i] + oneBigRow[3 + i] + oneBigRow[6 + i] == 3 ){  // Vertical win
+                return true
+            }
+        }
+        for(i in 0..1)
+        if(oneBigRow[0 + 2 * i] + oneBigRow[4] + oneBigRow[8 - i * 2] == 3){  // Diagonal win
+            return true
+        }
+
+
+        return false
     }
 
     private fun onUpdatedGame(state: Game?, errorCode:Int?){
